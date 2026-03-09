7598e7f7b12b2a42205ac32fc91e92f1fc7df43c
Edwin Alexander Vargas <edwinalexandervargas07@gmail.com>
2026-03-08 21:49:58 -0700

    email js complete

--- index.js
+++ index.js
@@ -4,5 +4,22 @@
 
 function contact(event) {
     event.preventDefault();
-    console.log('It worked')
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classlist += " modal__overlay--visible"
+    emailjs
+        .sendForm(
+            'service_yep9wjg',
+            'template_7lhjvjq',
+            event.target,
+            'dbLyKxuFSb-dZwgDR'
+        ).then(() => {
+            loadingoading.classList.remove("modal__overlay--visible");
+            success.classList += " modal__overlay--visible";
+        }).catch(() => {
+            loading.classList.remove("modal__overlay--visible")
+            alert(
+                "The email service is temporarily unavailable. Please contact me directly on edwinalexandervargas07@gmail.com"
+            );
+        })
 }
